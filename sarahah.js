c0948cda4b44439db7bb9e3579aad672cec92b93
Leonardo Alves <leunardosevla@gmail.com>
2017-07-29 22:34:49 -0300

    Added delay between messages

--- sarahah.js
+++ sarahah.js
@@ -5,19 +5,35 @@ var tokenInit = fn.indexOf('value');
 var tokenEnd = fn.indexOf('value', tokenInit + 1);
 var strToken = fn.slice(tokenInit, tokenEnd);
 var token = strToken.split(`"`)[1];
+var nMsgSent = 0;
+var interval;
 
 // ------- Altere aqui ---------- //
 // ------- Change here ---------- //
+
+// Especifique aqui a mensagem
+// Especify here the message to be sent
 var texto = 'Haha';
+
 // Especifique aqui o numero de mensagens, não coloque muito pro seu ip não ser bloqueado
 // Number of messages, take care to not put a very high number for your ip not to be blocked
 var numeroMensagens = 10;
+
+// Especifique aqui o atraso entre cada mensagem em SEGUNDOS. 0 para sem atraso.
+// Especify here the delay between messages in SECONDS. 0 for none.
+var delay = 0; 
+
+// --------------------------- //
 // --------------------------- //
 
 SendMessage = function SendMessage() {
     var text = texto;
     var userId = $('#RecipientId').val(); 
-    
+    nMsgSent++;
+
+    if(numeroMensagens === nMsgSent) 
+        clearInterval(interval);
+
     $.ajax({
         url: '/Messages/SendMessage',
         type: 'POST',
@@ -32,4 +48,9 @@ SendMessage = function SendMessage() {
     });
 }
 
-for (i = 0; i < numeroMensagens; i++) SendMessage();
+if (delay === 0)
+    for (i = 0; i < numeroMensagens; i++)
+        SendMessage();
+else {
+    interval = setInterval(SendMessage, delay * 1000);
+}
